d7fc04e4bf09988ccb6d641fb800bf8722ca1b81
Alex Fedorchenko <mralex.fed@gmail.com>
2021-04-27 19:37:15 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.9803d6c559ab27cff409d8ba4b1914d7.js"
 );
 
 self.addEventListener('message', (event) => {
